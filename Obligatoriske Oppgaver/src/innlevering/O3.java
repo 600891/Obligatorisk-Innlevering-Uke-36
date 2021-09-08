92ad60e10462edf2dc96983f02a756a51951a68f
NoraLaptop <600891@stud.hvl.no>
2021-09-08 16:15:29 +0200

    failcheck

--- Obligatoriske Oppgaver/src/innlevering/O3.java
+++ Obligatoriske Oppgaver/src/innlevering/O3.java
@@ -4,7 +4,7 @@ import static javax.swing.JOptionPane.*;
 public class O3 {
 	public static void main (String[] args) {
 		int n = Integer.parseInt(showInputDialog("Enter a whole number greater than 0."));
-		if (n <= 0) {
+		while (n <= 0) {
 			n = Integer.parseInt(showInputDialog("Number is too small, try again."));
 		}
 		int resultat = 1;
